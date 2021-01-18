6b9da0b5ddf5966c7d8ea400132dfdd938875b5c
Nneka Ufumaka <ufumakanneka@gmail.com>
2021-01-17 21:07:00 -0500

    commit tree a9f78522c3fe27bd79bad0e7945bc3568e46d0b2

--- lib/hello.rb
+++ lib/hello.rb
@@ -1,6 +1,19 @@
-def hello_t
+def hello_t(array)
+    if block_given?
+        i = 0
 
+        while i < array.length
+            yield (array[i])  #what does this line mean? we are passing the argument of array and naming each element inside the argument "i". why are using parenthesis here optional?
+            i = i + 1
+   
+        end
+    array 
+    else
+        puts "Hey! No block was given!"
+    end 
 end
 
-# call your method here!
+
+
+#hello_t(i) { puts array}
 
